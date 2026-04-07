9c047e43bcc21614f32bd50bcfdb763415a51d96
Gustavo Vázco <gustavovazco@gmail.com>
2026-04-07 11:59:11 -0600

    🐛 FIX: update photo name

--- dist/server/entry.mjs
+++ dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CPDDAyc2.mjs';
-import { manifest } from './manifest_RJNgOBve.mjs';
+import { manifest } from './manifest_BxaWEagB.mjs';
 
 const serverIslandMap = new Map();;
 
